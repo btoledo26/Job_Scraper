edf15aaccd6eb4ef089b9b9e81c9f5d1773a9c0c
Brandon <59942879+btoledo26@users.noreply.github.com>
2023-06-29 15:47:44 -0700

    scraper now works on Indeed, shoudld move from class to functions

--- scraper.py
+++ scraper.py
@@ -1,142 +1,156 @@
-import os.path
-import csv
+from csv import writer
 from bs4 import BeautifulSoup
+from lxml import etree as et
 from selenium import webdriver
-from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.ui import WebDriverWait as Wait
-import pandas as pd
 import time
 
 
 class Scraper:
     def __init__(self):
         options = Options()
-        # options.add_argument("-headless")
+        options.add_argument("-headless")
         self.driver = webdriver.Firefox(options=options)
-        self.proxies = []
-        self.filepath = "jobListings.csv"
+        self.filepath = 'indeed_jobs.csv'
+        self.job_search_keyword = 'Software+Engineer'
+        self.location_search_keyword = 'Oregon'
+        self.indeed_base_url = 'https://www.indeed.com'
+        self.indeed_pagination_url = "https://www.indeed.com/jobs?q={}&l={}&radius=35&start={}"
 
     def scrape(self) -> None:
-        print('Setting up file header')
-        self.write_csv_header()
-
         print('Scraping...')
+
         # add all scrape functions here
         self.scrape_indeed()
 
-        self.remove_duplicates()
-        self.cleanup()
+        print('Scraping Complete')
 
-    def use_proxy(self) -> None:
-        print('Gathering proxy addresses')
-
-        self.__gather_proxies()
-        # set up proxy server rotation
-
-        print('Now using proxy')
-        pass
-
-    def __gather_proxies(self) -> None:
-        proxy_url = "https://sslproxies.org/"
-        self.driver.get(proxy_url)
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.TAG_NAME, 'tbody')))
-        table_body = self.driver.find_element(By.TAG_NAME, 'tbody')
-        table_rows = table_body.find_elements(By.TAG_NAME, 'tr')
-
-        for row in table_rows:
-            ip = row.find_elements(By.TAG_NAME, 'td')[0].text
-            port = row.find_elements(By.TAG_NAME, 'td')[1].text
-            self.proxies.append(ip+':'+port)
-
-        # print(self.proxies)
-        print('Proxy addresses gathered successfully')
-
-    def __scroll_down_page(self, speed=8):
-        current_scroll_position, new_height = 0, 1
-        while current_scroll_position <= new_height:
-            current_scroll_position += speed
-            self.driver.execute_script("window.scrollTo(0, {});".format(current_scroll_position))
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
-
-        # TODO: Set to private when complete
     def scrape_indeed(self) -> None:
-        print('Scraping Indeed.com')
+        # Open a CSV file to write the job listings data
+        with open(self.filepath, 'w', newline='', encoding='utf-8') as f:
+            file_writer = writer(f)
+            heading = ['URL', 'Job Title', 'Company Name', 'Location', 'Salary', 'Job Type', 'Rating',
+                       'Job Description', 'Searched Job', 'Searched Location']
+            file_writer.writerow(heading)
 
-        # TODO: Increment page number?
-        page_number = 0
-        url = "https://www.indeed.com/jobs?q=software+engineer&l=Oregon&start=%s" % page_number
-        self.driver.get(url)
+            all_jobs = []
+            for page_no in range(0, 5):
+                url = self.indeed_pagination_url.format(self.job_search_keyword, self.location_search_keyword, page_no)
+                page_dom = self.__get_dom(url)
+                jobs = page_dom.xpath('//div[@class="job_seen_beacon"]')
+                all_jobs = all_jobs + jobs
+                time.sleep(5)
 
-        # Scroll page to load elements
-        self.__scroll_down_page()
+            for job in all_jobs:
+                job_link = self.indeed_base_url + self.__get_job_link(job)
+                time.sleep(2)
+                job_title = self.__get_job_title(job)
+                time.sleep(2)
+                company_name = self.__get_company_name(job)
+                time.sleep(2)
+                company_location = self.__get_company_location(job)
+                time.sleep(2)
+                salary = self.__get_salary(job)
+                time.sleep(2)
+                job_type = self.__get_job_type(job)
+                time.sleep(2)
+                rating = self.__get_rating(job)
+                time.sleep(2)
+                job_desc = self.__get_job_desc(job)
+                time.sleep(2)
+                record = [job_link, job_title, company_name, company_location, salary, job_type, rating,
+                          job_desc, self.job_search_keyword, self.location_search_keyword]
+                file_writer.writerow(record)
 
-        WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "companyName")))
-        WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "jcs-JobTitle")))
-
-        joblist = self.driver.find_element(By.CLASS_NAME, "jobsearch-ResultsList")
-        jobs = joblist.find_elements(By.TAG_NAME, 'li')
-
-        print(len(jobs)) #Temp print
-
-        # Parse job listings and store in list of rows to be written to file
-        rows = []
-        for job in jobs[:10]:
-            job_title = job.find_element(By.CLASS_NAME, "jobTitle").text
-            company = job.find_element(By.CLASS_NAME, "companyName").text
-            location = job.find_element(By.CLASS_NAME, "companyLocation").text
-            url = job.find_element(By.CLASS_NAME, "jcs-JobTitle").get_attribute('href')
-
-            rows.append(['', job_title, company, location, url])
-
-            print(job_title)  # Title
-            print(company)  # Company
-            print(location)
-            print(url)
-            print()
-
-        # Write job listings to csv file
-        file = open(self.filepath, 'a', newline='')
-        write = csv.writer(file, dialect='excel')
-        write.writerows(rows)
-        file.close()
-
-    # TODO: Set to private when complete
-    def write_csv_header(self) -> None:
-        # checks if file exists before writing headers
-        if not os.path.isfile(self.filepath):
-            col_names = ['Applied',
-                         'Job Title',
-                         'Company Name',
-                         'Company Location',
-                         'URL']
-            f = open(self.filepath, 'w', newline='')
-            writer = csv.writer(f, dialect='excel')
-            writer.writerow(col_names)
-            f.close()
-
-    # TODO: Set to private when complete
-    def remove_duplicates(self) -> None:
-        df = pd.read_csv(self.filepath)
-        df.drop_duplicates(inplace=True)
-        df.to_csv(self.filepath, index=False)
-
-    # TODO: Set to private when complete
-    def cleanup(self) -> None:
+        # Closing the web browser
         self.driver.quit()
+
+    def __get_dom(self, url):
+        self.driver.get(url)
+        page_content = self.driver.page_source
+        product_soup = BeautifulSoup(page_content, 'html.parser')
+        dom = et.HTML(str(product_soup))
+        return dom
+
+    # functions to extract job link
+    def __get_job_link(self, job):
+        try:
+            job_link = job.xpath('./descendant::h2/a/@href')[0]
+        except Exception as e:
+            job_link = 'Not available'
+        return job_link
+
+    # functions to extract job title
+    def __get_job_title(self, job):
+        try:
+            job_title = job.xpath('./descendant::h2/a/span/text()')[0]
+        except Exception as e:
+            job_title = 'Not available'
+        return job_title
+
+    # functions to extract the company name
+    def __get_company_name(self, job):
+        try:
+            company_name = job.xpath('./descendant::span[@class="companyName"]/text()')[0]
+        except Exception as e:
+            company_name = 'Not available'
+        return company_name
+
+    # functions to extract the company location
+    def __get_company_location(self, job):
+        try:
+            company_location = job.xpath('./descendant::div[@class="companyLocation"]/text()')[0]
+        except Exception as e:
+            company_location = 'Not available'
+        return company_location
+
+    # functions to extract salary information
+    def __get_salary(self, job):
+        try:
+            salary = job.xpath('./descendant::span[@class="estimated-salary"]/span/text()')
+        except Exception as e:
+            salary = 'Not available'
+        if len(salary) == 0:
+            try:
+                salary = job.xpath('./descendant::div[@class="metadata salary-snippet-container"]/div/text()')[0]
+            except Exception as e:
+                salary = 'Not available'
+        else:
+            salary = salary[0]
+        return salary
+
+    # functions to extract job type
+    def __get_job_type(self, job):
+        try:
+            job_type = job.xpath('./descendant::div[@class="metadata"]/div/text()')[0]
+        except Exception as e:
+            job_type = 'Not available'
+        return job_type
+
+    # functions to extract job rating
+    def __get_rating(self, job):
+        try:
+            rating = job.xpath('./descendant::span[@class="ratingNumber"]/span/text()')[0]
+        except Exception as e:
+            rating = 'Not available'
+        return rating
+
+    # functions to extract job description
+    def __get_job_desc(self, job):
+        try:
+            job_desc = job.xpath('./descendant::div[@class="job-snippet"]/ul/li/text()')
+        except Exception as e:
+            job_desc = ['Not available']
+        if job_desc:
+            job_desc = ",".join(job_desc)
+        else:
+            job_desc = 'Not available'
+        return job_desc
 
 
 def main():
     web_scraper = Scraper()
-    # web_scraper.gather_proxies()
-    web_scraper.write_csv_header()
-    web_scraper.scrape_indeed()
-
-    web_scraper.remove_duplicates()
-    web_scraper.cleanup()
+    web_scraper.scrape()
 
 
 if __name__ == "__main__":
