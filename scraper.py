cddebfb0046385315e6f1849ca6a7f8f09a686e0
Brandon <59942879+btoledo26@users.noreply.github.com>
2023-07-02 16:06:18 -0700

    now using page traversal buttons to navigate site, testing

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from csv import writer
 import pandas as pd
 from bs4 import BeautifulSoup
 from lxml import etree as et
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
 
 def scrape(driver, job_search_keyword, location_search_keyword) -> None:
@@ -65,18 +67,7 @@ def scrape_glassdoor(driver, job_search_keyword, location_search_keyword) -> Non
     print('Scraping Glassdoor.com...')
 
     glassdoor_base_url = 'https://www.glassdoor.com'
-    # glassdoor_pagination_url = 'https://www.glassdoor.com/Job/{}-{}-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP{}.htm'
-
-    # Site has semi-random urls, using hard-coded values for now
-    urls = [
-        'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24.htm?includeNoSalaryJobs=true',
-        'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP2.htm?includeNoSalaryJobs=true&pgc=AB4AAYEAHgAAAAAAAAAAAAAAAgik1rkAOgEBAQc7ryf1%2FLE0RCdD9KqNWWmG6wJuapjXX%2BxaMef0Bre9tdWb1ZAQ%2BOMaIRF98ZjoGsn3i4McmqUAAA%3D%3D'
-        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP3.htm?includeNoSalaryJobs=true&pgc=AB4AAoEAPAAAAAAAAAAAAAAAAgik1rkAVAEDAQcSBg4Na704hH503YQFZm%2FKgW6fF0H432wC0s7m6bbQgOdlV5HVG9dErW5nx5zxNpvqMssukBNVKQsUjMHtUdl5FNbBDWdDvCxDDK8NIpLCdgAA',
-        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP4.htm?includeNoSalaryJobs=true&pgc=AB4AA4EAWgAAAAAAAAAAAAAAAgik1rkAcQECAQ8gEAZ%2FHzybenWzvqh1hJOOofPAdKFRSyBBXwSxDoExaTxkPfH9O%2F7ZIYIxUWrGPr4HVOUUs7yh0OEmy7wIGRimVKx%2Fqks4dg9%2BsV6aSvtlu4r5bPxu%2F%2BSaLWBQ67J%2BMP71Lu5dIW0przs6BEqNAAA%3D',
-        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP5.htm?includeNoSalaryJobs=true&pgc=AB4ABIEAeAAAAAAAAAAAAAAAAgik1rkAfgEEAQ8gEC4GHgcB2ILgyQ3ttCpulyUVwWUH8fqvssk0Op1j%2F1on9%2BgjYlKQ2I66y3pICwYCnZu7E0K4iuEVEZ%2BVN8JgmmjcjtXYfq7KcpOi54gMCXG4UA0R%2FBAPHXgh4SYYHwpZf0nv8aI%2FKBKGR1AWX0LqfMRV8qrRmtmiOgAA',
-        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP6.htm?includeNoSalaryJobs=true&pgc=AB4ABYEAlgAAAAAAAAAAAAAAAgik1rkAhAEDAR9EEiYJE3ZIk9fKmUFbbxfyVc1HmbDpfBrG3rsoZEh4ng0eI%2FupJDKIyCB7YYtbVqcKoVZ8FS9YXj9NpRRG2Cy0lrcG3eCNvr4H5juoKax%2FNcAsEbNAsiLGp7%2BAvFdOCOU%2BY9T0FogxFXRb508JpCDAJRTtUX44mxgth%2F32nnoxQAAA',
-        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP7.htm?includeNoSalaryJobs=true&pgc=AB4ABoEAtAAAAAAAAAAAAAAAAgik1rkAiAEDAR9AHpoBBiwiJrcB9xpc%2FQ7NAm%2FDjhnorbeCLLf2vS4ORF9JrbnImRY%2Br2ZLDZ%2FYMXHvfMFlDG0UekCVytAnMGXwMnNHCeRjkmTjcGuC2f0Ob9lNkx3jh8P1VSDTmtdpFRmtX8H2w0NhoTWEQ4Z6FGyjI7dRpU7q5nv0090shK9ExjNeW%2BMAAA%3D%3D'
-        ]
+    glassdoor_start_url = 'https://www.glassdoor.com/Job/{}-{}-jobs-SRCH_IL.0,6_IS3163_KO7,24.htm'
 
     # Open a CSV file to write the job listings data
     with open('glassdoor_jobs.csv', 'w', newline='', encoding='utf-8') as f:
@@ -84,12 +75,17 @@ def scrape_glassdoor(driver, job_search_keyword, location_search_keyword) -> Non
         heading = ['URL', 'Job Title', 'Company Name', 'Location', 'Salary', 'Searched Job', 'Searched Location']
         file_writer.writerow(heading)
 
+        page_dom = __get_dom(driver, glassdoor_start_url.format(location_search_keyword, job_search_keyword))
         all_jobs = []
-        for url in urls:  # site seems to list duplicates after 6th page
-            page_dom = __get_dom(driver, url)
+        for i in range(1, 7):  # site seems to list duplicates after 6th page
             jobs = page_dom.xpath('//div[@class="job-search-3x5mv1"]')
             all_jobs = all_jobs + jobs
             time.sleep(5)
+            driver.find_element(By.CLASS_NAME, 'nextButton').click()
+            
+            if i == 1:
+                time.sleep(5)
+                driver.find_element(By.CLASS_NAME, 'e1jbctw80').click()
 
         # Organize data and write it to file
         for job in all_jobs:
