16ad56ea5ab5bddc39e4120c6c13a28baa6d06ba
Brandon <59942879+btoledo26@users.noreply.github.com>
2023-07-02 15:20:43 -0700

    found issue with glassdoor scraping, swapping branches to correct this

--- scraper.py
+++ scraper.py
@@ -31,7 +31,7 @@ def scrape_indeed(driver, job_search_keyword, location_search_keyword) -> None:
 
         # Scrape data from pages, 0-based indexing
         all_jobs = []
-        for page_no in range(0, 11):
+        for page_no in range(0, 11):  # TODO: pull number of pages and use for range
             url = indeed_pagination_url.format(job_search_keyword, location_search_keyword, page_no)
             page_dom = __get_dom(driver, url)
             jobs = page_dom.xpath('//div[@class="job_seen_beacon"]')
@@ -65,7 +65,18 @@ def scrape_glassdoor(driver, job_search_keyword, location_search_keyword) -> Non
     print('Scraping Glassdoor.com...')
 
     glassdoor_base_url = 'https://www.glassdoor.com'
-    glassdoor_pagination_url = 'https://www.glassdoor.com/Job/{}-{}-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP{}.htm'
+    # glassdoor_pagination_url = 'https://www.glassdoor.com/Job/{}-{}-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP{}.htm'
+
+    # Site has semi-random urls, using hard-coded values for now
+    urls = [
+        'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24.htm?includeNoSalaryJobs=true',
+        'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP2.htm?includeNoSalaryJobs=true&pgc=AB4AAYEAHgAAAAAAAAAAAAAAAgik1rkAOgEBAQc7ryf1%2FLE0RCdD9KqNWWmG6wJuapjXX%2BxaMef0Bre9tdWb1ZAQ%2BOMaIRF98ZjoGsn3i4McmqUAAA%3D%3D'
+        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP3.htm?includeNoSalaryJobs=true&pgc=AB4AAoEAPAAAAAAAAAAAAAAAAgik1rkAVAEDAQcSBg4Na704hH503YQFZm%2FKgW6fF0H432wC0s7m6bbQgOdlV5HVG9dErW5nx5zxNpvqMssukBNVKQsUjMHtUdl5FNbBDWdDvCxDDK8NIpLCdgAA',
+        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP4.htm?includeNoSalaryJobs=true&pgc=AB4AA4EAWgAAAAAAAAAAAAAAAgik1rkAcQECAQ8gEAZ%2FHzybenWzvqh1hJOOofPAdKFRSyBBXwSxDoExaTxkPfH9O%2F7ZIYIxUWrGPr4HVOUUs7yh0OEmy7wIGRimVKx%2Fqks4dg9%2BsV6aSvtlu4r5bPxu%2F%2BSaLWBQ67J%2BMP71Lu5dIW0przs6BEqNAAA%3D',
+        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP5.htm?includeNoSalaryJobs=true&pgc=AB4ABIEAeAAAAAAAAAAAAAAAAgik1rkAfgEEAQ8gEC4GHgcB2ILgyQ3ttCpulyUVwWUH8fqvssk0Op1j%2F1on9%2BgjYlKQ2I66y3pICwYCnZu7E0K4iuEVEZ%2BVN8JgmmjcjtXYfq7KcpOi54gMCXG4UA0R%2FBAPHXgh4SYYHwpZf0nv8aI%2FKBKGR1AWX0LqfMRV8qrRmtmiOgAA',
+        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP6.htm?includeNoSalaryJobs=true&pgc=AB4ABYEAlgAAAAAAAAAAAAAAAgik1rkAhAEDAR9EEiYJE3ZIk9fKmUFbbxfyVc1HmbDpfBrG3rsoZEh4ng0eI%2FupJDKIyCB7YYtbVqcKoVZ8FS9YXj9NpRRG2Cy0lrcG3eCNvr4H5juoKax%2FNcAsEbNAsiLGp7%2BAvFdOCOU%2BY9T0FogxFXRb508JpCDAJRTtUX44mxgth%2F32nnoxQAAA',
+        #'https://www.glassdoor.com/Job/oregon-software-engineer-jobs-SRCH_IL.0,6_IS3163_KO7,24_IP7.htm?includeNoSalaryJobs=true&pgc=AB4ABoEAtAAAAAAAAAAAAAAAAgik1rkAiAEDAR9AHpoBBiwiJrcB9xpc%2FQ7NAm%2FDjhnorbeCLLf2vS4ORF9JrbnImRY%2Br2ZLDZ%2FYMXHvfMFlDG0UekCVytAnMGXwMnNHCeRjkmTjcGuC2f0Ob9lNkx3jh8P1VSDTmtdpFRmtX8H2w0NhoTWEQ4Z6FGyjI7dRpU7q5nv0090shK9ExjNeW%2BMAAA%3D%3D'
+        ]
 
     # Open a CSV file to write the job listings data
     with open('glassdoor_jobs.csv', 'w', newline='', encoding='utf-8') as f:
@@ -73,10 +84,8 @@ def scrape_glassdoor(driver, job_search_keyword, location_search_keyword) -> Non
         heading = ['URL', 'Job Title', 'Company Name', 'Location', 'Salary', 'Searched Job', 'Searched Location']
         file_writer.writerow(heading)
 
-        # Scrape data from pages, 1-based indexing
         all_jobs = []
-        for page_no in range(1, 16):
-            url = glassdoor_pagination_url.format(location_search_keyword, job_search_keyword, page_no)
+        for url in urls:  # site seems to list duplicates after 6th page
             page_dom = __get_dom(driver, url)
             jobs = page_dom.xpath('//div[@class="job-search-3x5mv1"]')
             all_jobs = all_jobs + jobs
@@ -211,21 +220,21 @@ def __get_glassdoor_company_name(job):
 # Extract Glassdoor.com company location
 def __get_glassdoor_company_location(job):
     try:
-        job_title = job.xpath('./descendant::a/div/div[3]/text()')[0]
+        company_location = job.xpath('./descendant::a/div/div[3]/text()')[0]
     except Exception as e:
-        job_title = 'Not available'
+        company_location = 'Not available'
         logging.exception(e)
-    return job_title
+    return company_location
 
 
 # Extract Glassdoor.com salary information
 def __get_glassdoor_salary(job):
     try:
-        job_title = job.xpath('./descendant::a/div/div[4]/text()')[0]
+        salary = job.xpath('./descendant::a/div/div[4]/text()')[0]
     except Exception as e:
-        job_title = 'Not available'
+        salary = 'Not available'
         logging.exception(e)
-    return job_title
+    return salary
 
 
 def __remove_duplicates(filepath):
