b6aa399e0a3547c85e33dc59cc11878a82eb7793
Brandon <59942879+btoledo26@users.noreply.github.com>
2023-07-12 20:54:53 -0700

    moved main.py to its own directory to separate testing from the actual module

--- testing/__main__.py
+++ testing/__main__.py
@@ -3,8 +3,8 @@ from scraper import scrape
 
 def main():
     scrape('software engineer', 'oregon')
-    # scrape('software engineer', 'oregon', 1)
-    # scrape('software engineer', 'oregon', 2)
+    scrape('software engineer', 'oregon', 1)
+    scrape('software engineer', 'oregon', 2)
 
 
 if __name__ == "__main__":
